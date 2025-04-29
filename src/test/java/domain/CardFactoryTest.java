73a7faf060fc347e8e04fa850a2eb40aeafefa87
Ethan Pineda <joshuapineda66@gmail.com>
2025-04-29 16:33:47 -0500

    adding CreateCard_WithNullType_ThrowsNullPointerException test case

--- src/test/java/domain/CardFactoryTest.java
+++ src/test/java/domain/CardFactoryTest.java
@@ -1,4 +1,14 @@
 package domain;
 
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CardFactoryTest {
+
+    @Test
+    public void CreateCard_WithNullType_ThrowsNullPointerException() {
+        CardFactory factory = new CardFactory();
+        assertThrows(NullPointerException.class, () -> factory.createCard(null));
+    }
 }
