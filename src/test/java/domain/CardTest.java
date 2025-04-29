190b7e4e9ab25ae4278e833db9b483f40fd5d625
Ethan Pineda <joshuapineda66@gmail.com>
2025-04-29 13:20:38 -0500

    Constructor_WithValidCardType_CreatesCard fails

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -4,8 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CardTest {
+
     @Test
     public void Constructor_WithNullType_ThrowsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Card(null));
+    }
+
+    @Test
+    public void Constructor_WithValidCardType_CreatesCard() {
+        CardType type = CardType.EXPLODING_KITTEN;
+        Card card = new Card(type);
+        assertEquals(type, card.getType());
     }
 }
