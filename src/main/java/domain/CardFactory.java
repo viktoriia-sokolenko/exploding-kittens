4713ceb2eaa668aa377414e0566f266cb699557b
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-05-03 16:54:26 -0500

    fix: gradle build passes

--- src/main/java/domain/CardFactory.java
+++ src/main/java/domain/CardFactory.java
@@ -1,7 +1,9 @@
 package domain;
 
 public class CardFactory {
-    public void createCard(CardType cardType) {
-        throw new NullPointerException();
-    }
+	public void createCard(CardType cardType) {
+		if (cardType == null) {
+			throw new NullPointerException();
+		}
+	}
 }
--- src/test/java/domain/CardFactoryTest.java
+++ src/test/java/domain/CardFactoryTest.java
@@ -1,13 +1,13 @@
 package domain;
 
 import org.junit.jupiter.api.Test;
-import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CardFactoryTest {
-    @Test
-    public void CreateCard_WithNullType_ThrowsNullPointerException() {
-        CardFactory factory = new CardFactory();
-        assertThrows(NullPointerException.class, () -> factory.createCard(null));
-    }
+	@Test
+	public void CreateCard_WithNullType_ThrowsNullPointerException() {
+		CardFactory factory = new CardFactory();
+		assertThrows(NullPointerException.class, () -> factory.createCard(null));
+	}
 }
