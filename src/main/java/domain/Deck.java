9707b52bfcb9482b07b57bae7020d3860d764d8c
PaulaFregene <paulafregene@gmail.com>
2025-05-03 20:57:37 -0500

    Draw_WithOneCard_ReturnsCardWithSizeNowZero and Draw_WithEmptyDeck_ThrowsNoSuchElementException passes

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,10 +1,20 @@
 package domain;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class Deck {
+	private List<Card> deck;
 
-	public int draw() {
-		throw new NoSuchElementException("Deck is empty");
+	public Deck(List<Card> cardList) {
+		this.deck = cardList;
+	}
+
+	public Card draw() {
+		if (this.deck.isEmpty()){
+			throw new NoSuchElementException("Deck is empty");
+		}
+
+		return this.deck.remove(this.deck.size() - 1);
 	}
 }
--- src/test/java/domain/DeckTest.java
+++ src/test/java/domain/DeckTest.java
@@ -1,7 +1,10 @@
 package domain;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,7 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class DeckTest {
 	@Test
 	public void Draw_WithEmptyDeck_ThrowsNoSuchElementException() {
-		Deck deck = new Deck();
+		List<Card> emptyCardList = new ArrayList<>();
+		Deck deck = new Deck(emptyCardList);
 
 		String expectedMessage = "Deck is empty";
 
@@ -20,5 +24,20 @@ public class DeckTest {
 
 		String actualMessage = exception.getMessage();
 		assertEquals(expectedMessage, actualMessage);
+	}
+
+	@Test
+	public void Draw_WithOneCard_ReturnsCardWithSizeNowZero() {
+		CardType cardType = CardType.NORMAL;
+		Card card = new Card(cardType);
+		List<Card> cardList = new ArrayList<>(List.of(card));
+
+		Deck deck = new Deck(cardList);
+
+		Card actualCard = deck.draw();
+
+		assertEquals(card, actualCard);
+		assertEquals(0, cardList.size());
+
 	}
 }
