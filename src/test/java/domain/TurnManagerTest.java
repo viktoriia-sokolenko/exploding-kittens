fbe607eb0c15244782ac78a8f969d14e6f547bd2
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-02 16:18:05 -0500

    basic setup for turn manager test

---
+++ src/test/java/domain/TurnManagerTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class TurnManagerTest {
+
+    private TurnManager turnManager;
+    private Deck deck;
+    private PlayerManager playerManager;
+
+    @BeforeEach
+    void setUp() {
+        List<Card> cards = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            cards.add(new SkipCard()); // Placeholder for since this is only
+            // card that's basically implemented
+        }
+        deck = new Deck(cards);
+        turnManager = new TurnManager();
+
+        playerManager = new PlayerManager(deck);
+        playerManager.addPlayers(3);
+    }
+
+    
+}
