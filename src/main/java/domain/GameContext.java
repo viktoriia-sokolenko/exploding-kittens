76c30b6e5a5cfe7d83804e02d2feb28b91e9eba6
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-02 13:20:47 -0500

    cardEffect_canBeImplemented passes

---
+++ src/main/java/domain/GameContext.java
@@ -0,0 +1,8 @@
+package domain;
+
+public class GameContext {
+
+    public Player getCurrentPlayer() {
+        return null;
+    }
+}
---
+++ src/test/java/domain/CardEffectTests.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.easymock.EasyMock.*;
+
+public class CardEffectTests {
+
+    private GameContext mockGameContext;
+
+    @BeforeEach
+    void setUp() {
+        mockGameContext = EasyMock.createMock(GameContext.class);
+    }
+
+    @Test
+    void cardEffect_canBeImplemented() {
+        CardEffect testEffect = new CardEffect() {
+            @Override
+            public void execute(GameContext context) {
+                context.getCurrentPlayer();
+            }
+        };
+
+        expect(mockGameContext.getCurrentPlayer()).andReturn(null);
+        replay(mockGameContext);
+
+        assertDoesNotThrow(() -> testEffect.execute(mockGameContext));
+        verify(mockGameContext);
+    }
+}
