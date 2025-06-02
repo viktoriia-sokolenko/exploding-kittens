b3b915df14fb5e3d27d1fb741f8c0e62f72dcfb1
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-02 14:54:49 -0500

    constructor_withNullDeck_throwsNullPointerException passes

--- src/main/java/domain/PlayerManager.java
+++ src/main/java/domain/PlayerManager.java
@@ -1,4 +1,18 @@
 package domain;
 
+import java.util.*;
+
 public class PlayerManager {
+    private final Deck deck;
+    private final List<Player> players;
+
+    public PlayerManager(Deck deck) {
+        if (deck == null) {
+            throw new NullPointerException("Deck cannot be null");
+        }
+        this.deck = deck;
+        this.players = new ArrayList<>();
+    }
+
+
 }
