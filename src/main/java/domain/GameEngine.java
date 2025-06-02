34112e09449d9f4accf0cd6c49adf4845b7339ed
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-02 14:37:18 -0500

    playCard_withNullPlayer_throwsNullPointerException passes

--- src/main/java/domain/GameEngine.java
+++ src/main/java/domain/GameEngine.java
@@ -1,6 +1,15 @@
 package domain;
 
+import java.util.Objects;
 
 public class GameEngine {
+    private CardManager cardManager;
 
+    public GameEngine() {
+        this.cardManager = new CardManager();
+    }
+
+    public void playCard(Player player, Card card) {
+        Objects.requireNonNull(player, "Player cannot be null");
+    }
 }
