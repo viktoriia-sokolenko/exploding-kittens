1b9dec3d1c875242b5d3c5e3cd24bac3c7040d81
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-10 12:35:19 -0500

    playCard_withNullPlayer_throwsNullPointerException

--- src/test/java/domain/GameEngineTest.java
+++ src/test/java/domain/GameEngineTest.java
@@ -80,4 +80,16 @@ public class GameEngineTest {
                 null
         ));
     }
+
+    @Test
+    void playCard_withNullPlayer_throwsNullPointerException() {
+        SkipCard skipCard = new SkipCard();
+
+        NullPointerException exception = assertThrows(
+                NullPointerException.class,
+                () -> gameEngine.playCard(null, skipCard)
+        );
+
+        assertEquals("Player cannot be null", exception.getMessage());
+    }
 }
