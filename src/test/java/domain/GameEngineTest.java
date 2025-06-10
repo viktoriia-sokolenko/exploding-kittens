62440f0adb6ab93035b55dc1b34db3514665759b
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-10 12:21:04 -0500

    remove previous code for GameEngine

--- src/test/java/domain/GameEngineTest.java
+++ src/test/java/domain/GameEngineTest.java
@@ -2,58 +2,10 @@ package domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.easymock.EasyMock;
+import ui.UserInterface;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameEngineTest {
-	private GameEngine gameEngine;
-
-	@BeforeEach
-	void setUp() {
-		gameEngine = new GameEngine();
-	}
-
-	@Test
-	void playCard_withNullPlayer_throwsNullPointerException() {
-		SkipCard skipCard = new SkipCard();
-
-		NullPointerException exception = assertThrows(
-				NullPointerException.class,
-				() -> gameEngine.playCard(null, skipCard)
-		);
-
-		assertEquals("Player cannot be null", exception.getMessage());
-	}
-
-	@Test
-	void playCard_withNullCard_throwsNullPointerException() {
-		Hand hand = new Hand();
-		Player player = new Player(hand);
-
-		NullPointerException exception = assertThrows(
-				NullPointerException.class,
-				() -> gameEngine.playCard(player, null)
-		);
-
-		assertEquals("Card cannot be null", exception.getMessage());
-	}
-
-	@Test
-	void playCard_playerHasCard_executesCardEffect() {
-		Hand hand = new Hand();
-		Player player = new Player(hand);
-		SkipCard skipCard = new SkipCard();
-
-		gameEngine = new GameEngine(mockTurnManager, mockPlayerManager, mockDeck, mockUI);
-		hand.addCard(skipCard);
-		assertEquals(1, player.getCardTypeCount(CardType.SKIP));
-		mockTurnManager.endTurnWithoutDraw();
-		EasyMock.expectLastCall();
-		EasyMock.replay(mockTurnManager);
-		assertDoesNotThrow(() -> gameEngine.playCard(player, skipCard));
-
-		assertEquals(0, player.getCardTypeCount(CardType.SKIP));
-		EasyMock.verify(mockTurnManager);
-	}
-
 
 }
