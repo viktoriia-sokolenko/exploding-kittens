a877fee94b8f4e6666a5f33df627388cf7ff1a66
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-13 16:29:24 -0500

    parseCardType_withEmptyString_returnsNull passes

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -421,4 +421,17 @@ public class PlayerTest {
 
 		EasyMock.verify(mockHand);
 	}
+
+	@Test
+	public void parseCardType_withEmptyString_returnsNull() {
+		Hand mockHand = EasyMock.createMock(Hand.class);
+		EasyMock.expect(mockHand.parseCardType("")).andReturn(null);
+		EasyMock.replay(mockHand);
+
+		Player player = new Player(mockHand);
+		CardType result = player.parseCardType("");
+		assertNull(result);
+
+		EasyMock.verify(mockHand);
+	}
 }
