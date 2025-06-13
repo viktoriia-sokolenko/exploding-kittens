3507f2a72d2f5eccc0053724f0d4a896a96d4a6d
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-13 16:35:00 -0500

    hasEmptyHand_afterRemovingAllCards_returnsTrue passes

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -552,4 +552,16 @@ public class PlayerTest {
 
 		EasyMock.verify(mockHand);
 	}
+
+	@Test
+	public void hasEmptyHand_afterRemovingAllCards_returnsTrue() {
+		Hand mockHand = EasyMock.createMock(Hand.class);
+		EasyMock.expect(mockHand.isEmpty()).andReturn(true);
+		EasyMock.replay(mockHand);
+
+		Player player = new Player(mockHand);
+		assertTrue(player.hasEmptyHand());
+
+		EasyMock.verify(mockHand);
+	}
 }
