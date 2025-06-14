a14ddfeaaf970217b8ed8069beabf267562b465e
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-14 02:26:37 -0500

    main_successPath_callsInitializeAndLoopAndNoError

--- src/test/java/domain/GameEngineLoopTest.java
+++ src/test/java/domain/GameEngineLoopTest.java
@@ -5,7 +5,11 @@ import org.junit.jupiter.api.Test;
 import org.easymock.EasyMock;
 import ui.UserInterface;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.List;
 
@@ -22,7 +26,6 @@ public class GameEngineLoopTest {
 		public int processCount = 0;
 		public int checkCount = 0;
 
-
 		public TestableGameEngine(
 				TurnManager turnManager,
 				PlayerManager playerManager,
@@ -31,7 +34,8 @@ public class GameEngineLoopTest {
 				CardFactory cardFactory,
 				SecureRandom secureRandom
 		) {
-			super(turnManager, playerManager, deck, userInterface, cardFactory, secureRandom);
+			super(turnManager, playerManager, deck, userInterface, cardFactory,
+					secureRandom);
 		}
 
 		@Override
@@ -62,6 +66,32 @@ public class GameEngineLoopTest {
 		}
 	}
 
+	private static class TestableMainEngine extends GameEngine {
+		public boolean initCalled = false;
+		public boolean loopCalled = false;
+
+		public TestableMainEngine(
+				TurnManager turnManager,
+				PlayerManager playerManager,
+				Deck deck,
+				UserInterface userInterface,
+				CardFactory cardFactory,
+				SecureRandom secureRandom
+		) {
+			super(turnManager, playerManager, deck, userInterface, cardFactory, secureRandom);
+		}
+
+		@Override
+		public void initializeGame() {
+			initCalled = true;
+		}
+
+		@Override
+		public void runGameLoop() {
+			loopCalled = true;
+		}
+	}
+
 	private TurnManager mockTurnManager;
 	private UserInterface mockUI;
 	private PlayerManager mockPlayerManager;
@@ -71,18 +101,18 @@ public class GameEngineLoopTest {
 
 	@BeforeEach
 	public void setUp() {
-		mockTurnManager = EasyMock.createMock(TurnManager.class);
-		mockUI = EasyMock.createMock(UserInterface.class);
+		mockTurnManager   = EasyMock.createMock(TurnManager.class);
+		mockUI            = EasyMock.createMock(UserInterface.class);
 		mockPlayerManager = EasyMock.createMock(PlayerManager.class);
-		mockDeck = EasyMock.createMock(Deck.class);
-		mockFactory = EasyMock.createMock(CardFactory.class);
-		mockRandom = EasyMock.createMock(SecureRandom.class);
+		mockDeck          = EasyMock.createMock(Deck.class);
+		mockFactory       = EasyMock.createMock(CardFactory.class);
+		mockRandom        = EasyMock.createMock(SecureRandom.class);
 	}
 
 	@Test
-	public void runGameLoop_withEliminationThenQuit_invokesCorrectMethods
-			() throws Exception {
-		Player elim = EasyMock.createMock(Player.class);
+	public void runGameLoop_withEliminationThenQuit_invokesCorrectMethods()
+			throws Exception {
+		Player elim   = EasyMock.createMock(Player.class);
 		Player active = EasyMock.createMock(Player.class);
 
 		EasyMock.expect(elim.isInGame()).andReturn(false);
@@ -94,7 +124,8 @@ public class GameEngineLoopTest {
 		EasyMock.replay(mockTurnManager, mockUI, elim, active);
 
 		TestableGameEngine engine = new TestableGameEngine(
-				mockTurnManager, mockPlayerManager, mockDeck, mockUI, mockFactory, mockRandom
+				mockTurnManager, mockPlayerManager, mockDeck, mockUI,
+				mockFactory, mockRandom
 		);
 
 		engine.invokeRunGameLoop();
@@ -113,13 +144,15 @@ public class GameEngineLoopTest {
 		Player active = EasyMock.createMock(Player.class);
 
 		EasyMock.expect(active.isInGame()).andReturn(true);
-		EasyMock.expect(mockTurnManager.getCurrentActivePlayer()).andReturn(active);
+		EasyMock.expect(mockTurnManager.getCurrentActivePlayer())
+				.andReturn(active);
 		EasyMock.expect(mockUI.getUserInput()).andReturn("quit");
 
 		EasyMock.replay(mockTurnManager, mockUI, active);
 
 		TestableGameEngine engine = new TestableGameEngine(
-				mockTurnManager, mockPlayerManager, mockDeck, mockUI, mockFactory, mockRandom
+				mockTurnManager, mockPlayerManager, mockDeck, mockUI,
+				mockFactory, mockRandom
 		);
 
 		engine.invokeRunGameLoop();
@@ -133,27 +166,30 @@ public class GameEngineLoopTest {
 	}
 
 	@Test
-	public void
-	initializeGame_dealsCardsAddsDefuseInsertsExplodingKittensAndShowsHelp() {
+	public
+	void initializeGame_dealsCardsAddsDefuseInsertsExplodingKittensAndShowsHelp
+			() {
 		Player p1 = EasyMock.createMock(Player.class);
 		Player p2 = EasyMock.createMock(Player.class);
 		List<Player> players = List.of(p1, p2);
-		int playerCount = players.size();
+		int playerCount   = players.size();
 		int explodingCount = playerCount - ONE_CYCLE;
 
 		EasyMock.expect(mockPlayerManager.getPlayers()).andReturn(players);
 
-		p1.drawCard(mockDeck); EasyMock.expectLastCall()
-				.times(INITIAL_HAND_SIZE);
-		p2.drawCard(mockDeck); EasyMock.expectLastCall()
-				.times(INITIAL_HAND_SIZE);
+		p1.drawCard(mockDeck);
+		EasyMock.expectLastCall().times(INITIAL_HAND_SIZE);
+		p2.drawCard(mockDeck);
+		EasyMock.expectLastCall().times(INITIAL_HAND_SIZE);
 
 		Card defuseCard = EasyMock.createMock(Card.class);
 		EasyMock.expect(mockFactory.createCard(CardType.DEFUSE))
 				.andReturn(defuseCard).times(playerCount);
 
-		p1.addCardToHand(defuseCard); EasyMock.expectLastCall();
-		p2.addCardToHand(defuseCard); EasyMock.expectLastCall();
+		p1.addCardToHand(defuseCard);
+		EasyMock.expectLastCall();
+		p2.addCardToHand(defuseCard);
+		EasyMock.expectLastCall();
 
 		EasyMock.expect(mockDeck.getDeckSize()).andReturn(DECK_SIZE);
 		Card kittenCard = EasyMock.createMock(Card.class);
@@ -162,10 +198,11 @@ public class GameEngineLoopTest {
 
 		mockDeck.insertCardAt(EasyMock.eq(kittenCard), EasyMock.anyInt());
 		EasyMock.expectLastCall().times(explodingCount);
-		mockUI.displayHelp(); EasyMock.expectLastCall();
+		mockUI.displayHelp();
+		EasyMock.expectLastCall();
 
-		EasyMock.replay(mockPlayerManager, mockDeck, p1, p2, mockFactory,
-				mockUI);
+		EasyMock.replay(mockPlayerManager, mockDeck, p1, p2,
+				mockFactory, mockUI);
 
 		TestableGameEngine engine = new TestableGameEngine(
 				mockTurnManager, mockPlayerManager, mockDeck,
@@ -176,7 +213,39 @@ public class GameEngineLoopTest {
 
 		assertTrue(engine.getIsGameRunning());
 
-		EasyMock.verify(mockPlayerManager, mockDeck, p1, p2, mockFactory,
-				mockUI);
+		EasyMock.verify(mockPlayerManager, mockDeck, p1, p2,
+				mockFactory, mockUI);
 	}
+	@Test
+	public void main_successPath_callsInitializeAndLoopAndNoError()
+			throws Exception {
+		System.setIn(new ByteArrayInputStream("2\n"
+				.getBytes(StandardCharsets.UTF_8)));
+		System.setOut(new PrintStream(new ByteArrayOutputStream()));
+		EasyMock.replay(mockUI);
+
+		TestableMainEngine engine = new TestableMainEngine(
+				mockTurnManager, mockPlayerManager, mockDeck,
+				mockUI, mockFactory, mockRandom
+		);
+
+		getMainMethod().invoke(engine, (Object) new String[] {});
+
+		assertTrue(engine.initCalled,
+				"initializeGame() should have been called");
+		assertTrue(engine.loopCalled,
+				"runGameLoop() should have been called");
+
+		EasyMock.verify(mockUI);
+	}
+
+
+
+	private Method getMainMethod() throws Exception {
+		Method m = GameEngine.class.getDeclaredMethod(
+				"main", String[].class);
+		m.setAccessible(true);
+		return m;
+	}
+
 }
