78c707833186ea5730590fc3a4adead4be13ca93
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-16 01:32:30 -0500

    execute_nukeEffect_callsMoveAllExplodingKittensToTop and ran formatter

--- src/test/java/domain/NukeCardTest.java
+++ src/test/java/domain/NukeCardTest.java
@@ -7,27 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class NukeCardTest {
 
-    @Test
-    void createEffect_nukeCard_returnsNonNullEffect() {
-        NukeCard nukeCard = new NukeCard();
-        CardEffect effect = nukeCard.createEffect();
+	@Test
+	void createEffect_nukeCard_returnsNonNullEffect() {
+		NukeCard nukeCard = new NukeCard();
+		CardEffect effect = nukeCard.createEffect();
 
-        assertNotNull(effect, "Effect cannot be null");
-    }
+		assertNotNull(effect, "Effect cannot be null");
+	}
 
-    @Test
-    void execute_nukeEffect_callsMoveAllExplodingKittensToTop() {
-        NukeCard nukeCard = new NukeCard();
-        CardEffect nukeEffect = nukeCard.createEffect();
-        GameContext mockGameContext = EasyMock.createMock(GameContext.class);
+	@Test
+	void execute_nukeEffect_callsMoveAllExplodingKittensToTop() {
+		NukeCard nukeCard = new NukeCard();
+		CardEffect nukeEffect = nukeCard.createEffect();
+		GameContext mockGameContext = EasyMock.createMock(GameContext.class);
 
-        mockGameContext.moveAllExplodingKittensToTop();
-        EasyMock.expectLastCall();
-        EasyMock.replay(mockGameContext);
+		mockGameContext.moveAllExplodingKittensToTop();
+		EasyMock.expectLastCall();
+		EasyMock.replay(mockGameContext);
 
-        nukeEffect.execute(mockGameContext);
+		nukeEffect.execute(mockGameContext);
 
-        EasyMock.verify(mockGameContext);
-    }
+		EasyMock.verify(mockGameContext);
+	}
 
 }
