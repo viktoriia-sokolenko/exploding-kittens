a88e5cf2226af7f57ec79f3477f70428a704efc6
Ethan Pineda <joshuapineda66@gmail.com>
2025-06-16 01:30:29 -0500

    createEffect_nukeCard_returnsNonNullEffect

--- src/test/java/domain/NukeCardTest.java
+++ src/test/java/domain/NukeCardTest.java
@@ -1,4 +1,18 @@
 package domain;
 
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class NukeCardTest {
+
+    @Test
+    void createEffect_nukeCard_returnsNonNullEffect() {
+        NukeCard nukeCard = new NukeCard();
+        CardEffect effect = nukeCard.createEffect();
+
+        assertNotNull(effect, "Effect cannot be null");
+    }
+
 }
