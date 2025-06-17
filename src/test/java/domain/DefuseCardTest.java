9174cb7e77f66fd8b483c45dc7f25b7f23021f6d
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-06-16 21:19:36 -0500

    createEffect_defuseCard_returnsNullEffect passes

---
+++ src/test/java/domain/DefuseCardTest.java
@@ -0,0 +1,16 @@
+package domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class DefuseCardTest {
+	@Test
+	void createEffect_defuseCard_returnsNullEffect() {
+		DefuseCard defuseCard = new DefuseCard();
+		CardEffect effect = defuseCard.createEffect();
+
+		assertNull(effect);
+	}
+}
