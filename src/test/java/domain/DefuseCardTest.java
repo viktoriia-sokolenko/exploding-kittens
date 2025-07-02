082e20b06173b316301be8aa000f80dbaa2610a7
Your Name <viktoriia.i.sokolenko@gmail.com>
2025-07-02 16:44:22 +0300

    execute_defuseEffect_callsHandlePlayingDefuseCard passes

--- src/test/java/domain/DefuseCardTest.java
+++ src/test/java/domain/DefuseCardTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,5 +13,19 @@ public class DefuseCardTest {
 		CardEffect effect = defuseCard.createEffect();
 
 		assertNotNull(effect, "Effect cannot be null");
+	}
+
+	@Test
+	void execute_defuseEffect_callsHandlePlayingDefuseCard() {
+		DefuseCard defuseCard = new DefuseCard();
+		CardEffect defuseCardEffect = defuseCard.createEffect();
+
+		GameContext mockGameContext = EasyMock.createMock(GameContext.class);
+		mockGameContext.handlePlayingDefuseCard();
+		EasyMock.expectLastCall();
+		EasyMock.replay(mockGameContext);
+
+		defuseCardEffect.execute(mockGameContext);
+		EasyMock.verify(mockGameContext);
 	}
 }
